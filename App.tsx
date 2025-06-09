809f1c0c32b9d5d4263a613aeec5169ee0b88ff7
dung.nguyen30 <dung.nguyen30@ntq-solution.com.vn>
2025-06-09 15:03:35 +0700

    add husky

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ function App(): React.JSX.Element {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Text>Hoang Dung</Text>
+          <Tex>Hoang Dung</Tex>
           <LearnMoreLinks />
         </View>
       </ScrollView>
